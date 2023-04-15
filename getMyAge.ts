26f501cf05956caa530907e1f324bf67a3eca370
dzgierski19 <dzgierskigit@gmail.com>
2023-04-15 09:36:23 +0200

    added possibility to put string in date format as parameter

--- getMyAge.ts
+++ getMyAge.ts
@@ -2,31 +2,40 @@ const AGE_OF_THE_OLDEST_PERSON: number = 123;
 const CURRENT_YEAR: number = new Date().getFullYear();
 
 const getMyAge = (input: string | number | Date) => {
-  const inputTypeNumbered = checkInputType(input);
+  const inputTypeNumbered = checkAndConvertInputToNumber(input);
   if (CURRENT_YEAR - inputTypeNumbered > AGE_OF_THE_OLDEST_PERSON) {
     throw new Error("You are the oldest person in the world");
   }
   return CURRENT_YEAR - inputTypeNumbered;
 };
 
-const checkInputType = (input: string | number | Date) => {
+const checkAndConvertInputToNumber = (input: string | number | Date) => {
   if (typeof input === "string") {
-    return changeStringToNumber(input);
+    return changeStringToNumberOrDate(input);
   }
   if (input instanceof Date) {
-    return isNumberMoreThanCurrentYear(input.getFullYear());
+    return getValidYear(input.getFullYear());
   }
-  return isNumberMoreThanCurrentYear(input);
+  return getValidYear(input);
 };
 
-const changeStringToNumber = (input: string) => {
-  if (/^\d{4}$/.test(input) !== true) {
-    throw new Error("You must provide string with 4 digits");
+const changeStringToNumberOrDate = (input: string) => {
+  if (/^\d{4}$/.test(input) === true) {
+    return getValidYear(Number(input));
+  } else if (
+    /^\d{4}[ ]\d{2}[ ]\d{2}$/.test(input) === true ||
+    /^\d{4}[.]\d{2}[.]\d{2}$/.test(input) === true ||
+    /^\d{4}[/]\d{2}[/]\d{2}$/.test(input) === true ||
+    /^\d{4}[-]\d{2}[-]\d{2}$/.test(input) === true
+  ) {
+    return getValidYear(Number(input.slice(0, 4)));
   }
-  return isNumberMoreThanCurrentYear(Number(input));
+  throw new Error(
+    "You must provide string with 4 digits or in full date format"
+  );
 };
 
-const isNumberMoreThanCurrentYear = (input: number) => {
+const getValidYear = (input: number) => {
   isInputInteger(input);
   if (input > CURRENT_YEAR) {
     throw new Error("You are not born yet");
@@ -44,5 +53,9 @@ const result1 = getMyAge(new Date(2023, 1, 1));
 console.log(result1);
 const result2 = getMyAge(1993);
 console.log(result2);
-const result3 = getMyAge("1902");
+const result3 = getMyAge("1900/00/11");
 console.log(result3);
+
+// DODAĆ DATE I STRING DODATKOWE WYTYCZNE
+// dodać dzień i miesiąc
+// string RRRR-. /MM-. /DD
