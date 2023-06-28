86858b9bb6bc39a3dd0c853cbd66959063361593
dzgierski19 <dzgierskigit@gmail.com>
2023-06-28 17:19:05 +0200

    testing descriptions modified

--- src/test/getMyAge.test.ts
+++ src/test/getMyAge.test.ts
@@ -1,64 +1,65 @@
-import { it } from "node:test";
 import { getMyAge } from "../app/getMyAge";
 import { iterableEquality } from "@jest/expect-utils";
 
 describe("getMyAge test suite", () => {
   describe("testing input as date", () => {
-    test("should return proper age when valid date provided", () => {
+    it("should return proper age when valid date provided", () => {
       const result = getMyAge(new Date(1900, 6, 15));
       expect(result).toBe(122);
     });
-    test("should throw error when date is over 123 years ago", () => {
+  });
+  describe("testing input as number", () => {
+    it("should return proper age when valid year provided as number", () => {
+      const result = getMyAge(2000);
+      expect(result).toBe(23);
+    });
+  });
+  describe("testing input as String", () => {
+    it("should return proper age when valid year provided as string", () => {
+      const result = getMyAge("2000 06 30");
+      expect(result).toBe(22);
+    });
+  });
+  describe("should throw error when", () => {
+    it("- date is over 123 years ago", () => {
       function expectError() {
         getMyAge(new Date(1900, 5, 1));
       }
       expect(expectError).toThrow();
     });
-    test("should throw error when date is from the future", () => {
+    it("- date is from the future", () => {
       function expectError() {
         getMyAge(new Date(2024, 5, 1));
       }
       expect(expectError).toThrow();
     });
-  });
-  describe("testing input as number", () => {
-    test("should return proper age when valid year provided as number", () => {
-      const result = getMyAge(2000);
-      expect(result).toBe(23);
-    });
-    test("should throw error when provided year as number is from the future", () => {
-      function expectError() {
-        getMyAge(2024);
-      }
-      expect(expectError).toThrow();
-    });
-    test("should throw error when provided number and current year's difference is more than 123", () => {
+    it("- provided number and current year's difference is more than 123", () => {
       function expectError() {
         getMyAge(1899);
       }
       expect(expectError).toThrow();
     });
-    test("should throw error when provided number is not Integer", () => {
+    it("- provided year as number is from the future", () => {
+      function expectError() {
+        getMyAge(2024);
+      }
+      expect(expectError).toThrow();
+    });
+    it("- provided number is not Integer", () => {
       function expectError() {
         getMyAge(2022.5);
       }
       expect(expectError).toThrow();
     });
-  });
-  describe("testing input as String", () => {
-    test("should return proper age when valid year provided as string", () => {
-      const result = getMyAge("2000 06 30");
-      expect(result).toBe(22);
-    });
-    test("should throw error on string input that is not number", () => {
+    it("- provided invalid string input", () => {
       function expectError() {
-        getMyAge("date");
+        getMyAge("19999 22 22");
       }
       expect(expectError).toThrow();
     });
-    test("should throw error on invalid string input", () => {
+    it("- provided string input that is not number", () => {
       function expectError() {
-        getMyAge("19999 22 22");
+        getMyAge("date");
       }
       expect(expectError).toThrow();
     });
